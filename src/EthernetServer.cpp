8917d0c4200dd8b034d305adacad73c4012fa575
GuiGuerreiro <32772379+GuiGuerreiro@users.noreply.github.com>
2024-02-27 15:18:14 +0000

    Update EthernetServer.cpp

--- src/EthernetServer.cpp
+++ src/EthernetServer.cpp
@@ -25,7 +25,7 @@
 uint16_t EthernetServer::server_port[MAX_SOCK_NUM];
 
 
-void EthernetServer::begin()
+void EthernetServer::begin(uint16_t port)
 {
 	uint8_t sockindex = Ethernet.socketBegin(SnMR::TCP, _port);
 	if (sockindex < MAX_SOCK_NUM) {
